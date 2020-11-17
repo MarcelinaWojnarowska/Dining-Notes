a288b0e888a83da06d196e44f34a611faa52f42f
MarcelinaWojnarowska <sz.p.marcelina_w@wp.pl>
2020-11-17 17:28:37 +0100

    Local settings

--- dining_note/settings.py
+++ dining_note/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -24,9 +23,6 @@ SECRET_KEY = '455p3-im#qvub-2+_srrh1@a5clqbuo_z1kz6kp8m14%c6zi=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ['marcelinawojnarowska.pythonanywhere.com']
-
 
 # Application definition
 
@@ -70,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dining_note.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -80,7 +75,6 @@ DATABASES = {
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -100,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,9 +107,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You must be on production.')
